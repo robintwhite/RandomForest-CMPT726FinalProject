ab1845d816e2ef4bc651508ca54aa1269be27fde
rkm3 <rkm3@sfu.ca>
2017-11-25 03:57:32 -0800

    Added 3 new options to the CLI.
    -Added the following options:
    max_depth - the maximum depth of all trees in the random forest.
    min_split_size - the threshold number of samples required at a node to
                     stop further splitting.
    n_features - the number of features to be used when building each tree.

--- runner.py
+++ runner.py
@@ -38,8 +38,23 @@ def main():
         help="File containing the dataset.")
     argument_parser.add_argument(
         '-t', '--number_of_trees',
+        type=int,
         default=4,
         help="The number of trees to create for the random forest.")
+    argument_parser.add_argument(
+        '-m', '--max_depth',
+        type=int,
+        help="The maximum depth of all trees in the random forest.  (default: None).")
+    argument_parser.add_argument(
+        '-s', '--min_split_size',
+        type=int,
+        default=1,
+        help="The threshold number of samples required at a node to stop further splitting.  (default: 1).")
+    argument_parser.add_argument(
+        '-f', '--n_features',
+        type=int,
+        help="The number of features to use when building each tree in the random forest.  Specifying None will use all"
+              " the features (default: None).")
     argument_parser.add_argument(
         '-h', '--help',
         action='help',
