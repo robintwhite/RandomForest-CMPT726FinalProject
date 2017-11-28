df9df3bb16c80f60356bfe9728fc3ee44b0105a3
jla497 <jaehonglee86@gmail.com>
2017-11-27 21:24:45 -0800

    copied bug fixes into tree.py

--- Tree.py
+++ Tree.py
@@ -1,6 +1,3 @@
-from random import seed
-from random import randrange
-from math import sqrt
 import numpy as np
 
 class Tree():
@@ -32,7 +29,7 @@ class Tree():
         """
         print("I am tree number: {}".format(self.id))
 
-    def entropy_index(groups):
+    def entropy_index(self, groups):
         """
           Calculate information gain with entropy
         """
@@ -43,13 +40,13 @@ class Tree():
 
         for group in groups:
             # score the group based on the score for each class
-            score_t = entropy_grp_score(group)
-    
+            score_t = self.entropy_grp_score(group)
+
             entropy += (score_t) * (float(len(group)) / n_instances)
 
         return entropy
-    
-    def entropy_grp_score(group_t):
+
+    def entropy_grp_score(self, group_t):
 
         size = float(len(group_t))
             # avoid divide by zero
@@ -57,15 +54,15 @@ class Tree():
             return 0.0
 
         #count number of unique labels (column -1) and return total number of counts
-	
+
         labels, counts = np.unique(group_t[:,-1],return_counts=True)
 
         #score is equal to sum((each_label_count/size_group)^2)
         score_t = (np.log(counts/size,2)*(counts/size)).sum()*-1
 
         return score_t
-    
-    def gini_index(groups,num_labels):
+
+    def gini_index(self, groups, num_labels):
         """
         Calculate gini_index score for groups split based on whether sample's specific attribute
         value is greater than or equal to a chosen split point value
@@ -78,13 +75,13 @@ class Tree():
 
         for group in groups:
             # score the group based on the score for each class
-            score_t = gini_index_grp_score(group,num_labels)
+            score_t = self.gini_index_grp_score(group,num_labels)
 
             gini += (1.0 - score_t) * (float(len(group)) / n_instances)
 
         return gini
- 
-    def gini_index_grp_score(group_t,num_labels):
+
+    def gini_index_grp_score(self, group_t, num_labels):
 
         size = float(len(group_t))
             # avoid divide by zero
@@ -92,7 +89,7 @@ class Tree():
             return 0.0
 
         #count number of unique labels (column -1) and return total number of counts
-	
+
         labels, counts = np.unique(group_t[:,-1],return_counts=True)
 
         #score is equal to sum((each_label_count/size_group)^2)
@@ -101,7 +98,7 @@ class Tree():
         return score_t
 
     #optimized test_split function using numpy array.
-    def test_split(index,value,dataset_t):
+    def test_split(self, index, value, dataset_t):
         """
         Split dataset into groups less than or greater than
         an attribute value.
@@ -111,32 +108,32 @@ class Tree():
         right =dataset_t[dataset_t[:,index]>=value]
 
         return left, right
-    
-    def entropy_row_score(row,index,dataset,parent_entropy):
-        
+
+    def entropy_row_score(self, row, index, dataset, parent_entropy, num_labels):
+
         """
         get entropy_score for dataset split on each row's indexed attribute value
         """
-        
-        groups = test_split(index, row[index], dataset)
- 
-        entropy_score = parent_entropy + entropy_index(groups,num_labels) 
+
+        groups = self.test_split(index, row[index], dataset)
+
+        entropy_score = parent_entropy + self.entropy_index(groups,num_labels)
 
         return entropy_score
-    
-    
-    def gini_row_score(row,index,dataset,num_labels):
+
+
+    def gini_row_score(self, row, index, dataset, num_labels):
         """
         get gini_score for dataset split on each row's indexed attribute value
         """
-        groups = test_split(index, row[index], dataset)
-  
-        gini = gini_index(groups,num_labels)
+        groups = self.test_split(index, row[index], dataset)
+
+        gini = self.gini_index(groups,num_labels)
 
         return gini
- 
-   
-    def get_split(dataset, n_features,gini=True):
+
+
+    def get_split(self, dataset, n_features, gini=True):
         """
         Select the best split point for a dataset.
         This routine and related subroutines convert the dataset
@@ -145,42 +142,41 @@ class Tree():
         I think switching rest of the functions to use dataset in the numpy array format will improve runtime.
         """
         b_index, b_value, b_score, b_groups = 999, 999, 999, None
-
+        dataset_t = np.array(dataset)
         count_all_features = len(dataset[0])-1
-        
+        if not n_features:
+            n_features = count_all_features
+
         #count number of unique labels (column -1) and return total number of counts
-        labels, num_labels = np.unique(group_t[:,-1],return_counts=True)
+        labels, num_labels = np.unique(dataset_t[:,-1], return_counts=True)
 
         #randomly select number of features.
         #seed for testing
         #np.random.seed(7)
         features = np.random.randint(count_all_features,size=n_features)
 
-        dataset_t = np.array(dataset)
-
         for index in features:
-            
+
             scores = None
             #returns all gini index scores of each row for the selected feature
             if gini is True:
-                
-                scores = np.apply_along_axis(gini_row_score,1,dataset_t,index,dataset_t,num_labels)
+
+                scores = np.apply_along_axis(self.gini_row_score,1,dataset_t,index,dataset_t,num_labels)
             #returns all entropy scores of each row for the selected feature
             else:
-                
-                parent_entropy = entropy_index(dataset_t,num_labels)
-                
-                scores = np.apply_along_axis(entropy_row_score,1,dataset_t,index,dataset_t,parent_entropy)
-                
+
+                parent_entropy = self.entropy_index(dataset_t,num_labels)
+
+                scores = np.apply_along_axis(self.entropy_row_score,1,dataset_t,index,dataset_t,parent_entropy, num_labels)
+
             current_b_score = np.min(scores)
 
             current_b_row = np.argmin(scores)
 
-            groups = test_split(index, dataset_t[current_b_row,index], dataset_t)
-
             current_value = dataset_t[current_b_row,index]
 
             if current_b_score < b_score:
+                groups = self.test_split(index, dataset_t[current_b_row,index], dataset_t)
                 groups_t = []
 
                 #have to convert np array back to python list since rest of the functions take data as a list
