e97e6c752979c4baa3528a0865d759f038525126
rkm3 <rkm3@sfu.ca>
2017-11-30 13:00:30 -0800

    experiments-runner.sh can now run 3 experiments by varying different settings.
    experiments-runner.sh can now vary the 3 following parameters:
    1) The number of trees in the random forest.
    2) The number of features used to train each tree in the random forest.
    3) The max depth of the trees in the random forest.

--- experiments/scripts/experiments-runner.sh
+++ experiments/scripts/experiments-runner.sh
@@ -2,6 +2,10 @@
 # Script to run experiments for various settings of runner.py.
 
 # Experiment settings.
+MAX_TREES=10
+UPPER_DEPTH=10
+MAX_FEATURES_PER_TREE=10
+
 USE_GINI=0
 USE_ENTROPY=0
 USE_VARIANCE=0
@@ -9,6 +13,13 @@ USE_VARIANCE=0
 VARY_NUMBER_OF_TREES=0
 VARY_MAX_DEPTH=0
 VARY_FEATURES_PER_TREE=0
+
+# Experiment core settings.
+# TODO: Adjust these so they are the same as Weka and Scikit-Learn.
+NUMBER_OF_TREES=4
+MAX_DEPTH=5
+MIN_SPLIT_SIZE=2
+NUMBER_OF_FEATURES=13
 
 
 function usage() {
@@ -95,4 +106,51 @@ elif [[ ! -f $CSV_FILE ]]
 then
   echo "The provided file $CSV_FILE doesn't exist or isn't a file!"
   exit 1
+fi
+
+# Generate core command string.
+BASE_COMMAND="python ../../runner.py -d $CSV_FILE --use_hockey_preprocessor"
+
+if [[ $USE_GINI ]]
+then
+  BASE_COMMAND="$BASE_COMMAND --use_gini --target_label GP_greater_than_0"
+elif [[ $USE_ENTROPY ]]
+then
+  BASE_COMMAND="$BASE_COMMAND --use_entropy --target_label GP_greater_than_0"
+elif [[ $USE_VARIANCE ]]
+then
+  BASE_COMMAND="$BASE_COMMAND --use_variance --target_label sum_7yr_GP"
+fi
+
+echo -e "\nOkay, this part is probably going to take awhile so you will probably want to do something else in the" \
+        "meantime.\n"
+echo -e "TIP: Since each experiment uses a single process you might want to run the experiments in parallel.\n"
+
+if [[ $VARY_NUMBER_OF_TREES -eq 1 ]]
+then
+  echo -e "Running experiment to vary number of trees from 1 to $MAX_TREES...\n"
+  for value in $(seq 1 $MAX_TREES)
+  do
+    COMMAND="$BASE_COMMAND --max_depth $MAX_DEPTH --min_split_size $MIN_SPLIT_SIZE --n_features $NUMBER_OF_FEATURES --number_of_trees $value"
+    echo -e "Command: $COMMAND\n"
+    $COMMAND
+  done
+elif [[ $VARY_MAX_DEPTH -eq 1 ]]
+then
+  echo -e "Running experiment to vary max depth from 1 to $UPPER_DEPTH...\n"
+  for value in $(seq 1 $UPPER_DEPTH)
+  do
+    COMMAND="$BASE_COMMAND --max_depth $value --min_split_size $MIN_SPLIT_SIZE --n_features $NUMBER_OF_FEATURES --number_of_trees $NUMBER_OF_TREES"
+    echo -e "Command: $COMMAND\n"
+    $COMMAND
+  done
+elif [[ $VARY_FEATURES_PER_TREE -eq 1 ]]
+then
+  echo -e "Running experiment to vary number of features per tree from 1 to $MAX_FEATURES_PER_TREE...\n"
+  for value in $(seq 1 $MAX_FEATURES_PER_TREE...)
+  do
+    COMMAND="$BASE_COMMAND --max_depth $MAX_DEPTH --min_split_size $MIN_SPLIT_SIZE --n_features $value --number_of_trees $NUMBER_OF_TREES"
+    echo -e "Command: $COMMAND\n"
+    $COMMAND
+  done
 fi
